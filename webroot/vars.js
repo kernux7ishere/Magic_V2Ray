db24b47e69a329321d7dd3edfd5f50316edb3b50
vincentng295 <151985750+vincentng295@users.noreply.github.com>
2026-07-19 00:00:41 +0700

    Add default routing rules to advSettings for CN

--- webroot/vars.js
+++ webroot/vars.js
@@ -28,7 +28,96 @@ let advSettings = {
     vpnDns: "1.1.1.1",
     foreignDns: "1.1.1.1",
     domesticDns: "223.5.5.5",
-    routingRules: []
+    routingRules: [
+        {
+            "remarks": "阻断udp443",
+            "locked": false,
+            "domain": "",
+            "ip": "",
+            "port": "443",
+            "protocol": "",
+            "network": "udp",
+            "outboundTag": "block",
+            "enabled": true
+        },
+        {
+            "remarks": "代理Google",
+            "locked": false,
+            "domain": "geosite:google",
+            "ip": "",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "proxy",
+            "enabled": true
+        },
+        {
+            "remarks": "绕过局域网IP",
+            "locked": false,
+            "domain": "",
+            "ip": "geoip:private",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "direct",
+            "enabled": true
+        },
+        {
+            "remarks": "绕过局域网域名",
+            "locked": false,
+            "domain": "geosite:private",
+            "ip": "",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "direct",
+            "enabled": true
+        },
+        {
+            "remarks": "绕过中国公共DNSIP",
+            "locked": false,
+            "domain": "",
+            "ip": "223.5.5.5,223.6.6.6,2400:3200::1,2400:3200:baba::1,119.29.29.29,1.12.12.12,120.53.53.53,2402:4e00::,2402:4e00:1::,180.76.76.76,2400:da00::6666,114.114.114.114,114.114.115.115,114.114.114.119,114.114.115.119,114.114.114.110,114.114.115.110,180.184.1.1,180.184.2.2,101.226.4.6,218.30.118.6,123.125.81.6,140.207.198.6,1.2.4.8,210.2.4.8,52.80.66.66,117.50.22.22,2400:7fc0:849e:200::4,2404:c2c0:85d8:901::4,117.50.10.10,52.80.52.52,2400:7fc0:849e:200::8,2404:c2c0:85d8:901::8,117.50.60.30,52.80.60.30",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "direct",
+            "enabled": false
+        },
+        {
+            "remarks": "绕过中国公共DNS域名",
+            "locked": false,
+            "domain": "domain:alidns.com,domain:doh.pub,domain:dot.pub,domain:360.cn,domain:onedns.net",
+            "ip": "",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "direct",
+            "enabled": false
+        },
+        {
+            "remarks": "绕过中国IP",
+            "locked": false,
+            "domain": "",
+            "ip": "geoip:cn",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "direct",
+            "enabled": false
+        },
+        {
+            "remarks": "绕过中国域名",
+            "locked": false,
+            "domain": "geosite:cn",
+            "ip": "",
+            "port": "",
+            "protocol": "",
+            "network": "",
+            "outboundTag": "direct",
+            "enabled": false
+        }
+    ]
 };
 let currentLang = 'en';
 let currentEditingCategory = null;
